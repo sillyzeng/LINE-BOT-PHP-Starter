69d8fa75d72c85cd3b14e8433bc3b05f9c37d92c
sillyzeng <zeus_zeng@hotmail.com>
2017-02-01 22:42:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -61,4 +61,23 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK";
+echo 	$messages = [
+					  "type": "template",
+					  "altText": "this is a confirm template",
+					  "template": {
+					      "type": "confirm",
+					      "text": "Are you sure?",
+					      "actions": [
+						  {
+						    "type": "message",
+						    "label": "Yes",
+						    "text": "yes"
+						  },
+						  {
+						    "type": "message",
+						    "label": "No",
+						    "text": "no"
+						  }
+					      ]
+					  }
+				]; 
